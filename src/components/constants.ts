29dc512f4ab7e532414fe8274943379ba2bae971
Tina Lee <92625071+Learning-N-Running@users.noreply.github.com>
2023-09-27 14:51:16 +0900

    Chore

--- src/components/constants.ts
+++ src/components/constants.ts
@@ -3,3 +3,9 @@ export const constants = {
   SeopoliaRPCUrl:
     "https://eth-sepolia.g.alchemy.com/v2/S2XXpgSu1oKm4MLuk8F2BWpQ_UIjZX7l",
 };
+
+// export const constants = {
+//   ContractAddress: "0x806E846858E752eb9f709e9EB3803b9217fbe4d0",
+//   SeopoliaRPCUrl:
+//     "여기에_넣어주세요",
+// };
