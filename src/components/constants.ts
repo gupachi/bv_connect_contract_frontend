f64fbf640b8e111bbabce686d7ee484fb2b8b292
jiwon02 <86184712+gupachi@users.noreply.github.com>
2023-10-03 19:20:18 +0900

    css 추가

--- src/components/constants.ts
+++ src/components/constants.ts
@@ -1,5 +1,5 @@
 export const constants = {
-  ContractAddress: "0x806E846858E752eb9f709e9EB3803b9217fbe4d0",
+  ContractAddress: "",
   SeopoliaRPCUrl: "https://eth-sepolia.g.alchemy.com/v2/0QqrvtKHXTWDD9Fp2Aq0HiO_SeTvqCX8",
   ETHERSCAN_API_URL: "https://api.etherscan.io/api",
    API_KEY: "2PKECQ6H5VETVBBJZCVEAFWJIHBGVQ85FM",
